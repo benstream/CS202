6b00112bcc7dc9d286f29af33192712c98908523
Benjamin Stream <brstream@alaska.edu>
2021-01-21 10:28:23 -0900

    Lab 2 Setup

---
+++ Lab2/Lab 2/Lab 2/classes.cpp
@@ -0,0 +1,4 @@
+// Lab 2 Benjamin Stream
+// 1/21/20
+// CS202 UAF
+#include <\Users\Benjamin\source\repos\benstream\cs202\CS202\Lab2\Lab 2\Lab 2\classname.h>
---
+++ Lab2/Lab 2/Lab 2/main.cpp
@@ -0,0 +1,6 @@
+// Lab 2 Benjamin Stream
+// 1/21/20
+// CS202 UAF
+
+#include <iostream>;
+#include <vector>;
