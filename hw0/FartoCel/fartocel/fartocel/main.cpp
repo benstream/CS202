0feec25298fbbc0c98ce6cf833d2e055e9d9693c
Benjamin Stream <brstream@alaska.edu>
2021-01-26 11:14:08 -0900

    Initial Setup on main

---
+++ hw0/FartoCel/fartocel/fartocel/main.cpp
@@ -0,0 +1,16 @@
+/*
+Benjamin Stream
+CS 202
+HW 0
+1/26/21
+]*/
+
+#include <iostream>
+#include <string>
+using std::cout;
+using std::endl;
+using std::cin;
+using std::stod;
+using std::strtod;
+using std::string;
+
