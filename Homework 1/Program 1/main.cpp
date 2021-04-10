3f4f2b1fee04f9b4c10ff73cc6bb3dee3d6ee9ba
benstream <benjaminstream@yahoo.com>
2021-04-09 17:33:49 -0800

    Added stopSec and startSec into stopwatch class

--- Homework 1/Program 1/main.cpp
+++ Homework 1/Program 1/main.cpp
@@ -2,17 +2,25 @@
 using std::cout;
 using std::endl;
 using std::vector;
+using std::search;
+using std::binary_search;
 
 
+auto randGenerator() {
 
-std::vector<int> makeVector(int x,int y) {
-	std::vector<int> v;
 	// Will be used to obtain a seed for the random number engine.
 	std::random_device rd;
 
 	// Standard mersenne_twister_engine seeded with rd().
 	std::mt19937 gen(rd());
+	return gen;
+}
 
+
+vector<int> makeVector(int x,int y) {
+	vector<int> v;
+	
+	auto gen = randGenerator();
 	
 	for (int i = 0; i < y; i++) {
 		// Will create a vector based on the upper bounds of the random range.
@@ -28,12 +36,13 @@ std::vector<int> makeVector(int x,int y) {
 
 
 int main(int argc, const char** argv) {
-	StopWatch Default;
-	StopWatch TotalTime;
+	
+	StopWatch Time;
+	
 	//TotalTime.start();
 	//Default.start();
 
-	auto a = makeVector(0, 10); // creates a vector length 10 with random values 0-10
+	auto a = makeVector(0, 100000); // creates a vector length 10 with random values 0-10
 	//auto b = makeVector(0, 100);// creates a vector length 10^2 with random values 0-10^2
 	//auto c = makeVector(0, 1000);// creates a vector length 10^3 with random values 0-10^3
 	//auto d = makeVector(0, 10000);// creates a vector length 10^4 with random values 0-10^4
@@ -42,8 +51,9 @@ int main(int argc, const char** argv) {
 	//auto g = makeVector(0, 10000000);// creates a vector length 10^7 with random values 0-10^7
 	//auto h = makeVector(0, 100000000);// creates a vector length 10^8 with random values 0-10^8
 
-	
-
+	Time.start();
+	sort(a.begin(), a.end(), [](int a, int b) {return a > b; }); // Sorts by decending order
+	cout << Time.stop() << endl;
 	
 	
 	//cout << Default.stop() << endl;
--- Homework 1/Program 1/stopwatch.cpp
+++ Homework 1/Program 1/stopwatch.cpp
@@ -43,3 +43,32 @@ double StopWatch::stop()
 	else return 0;
 }
 
+double StopWatch::startSec()
+{
+	if (_running == false) {
+		_StartTime = std::chrono::steady_clock::now();
+		_running = true;
+		return 1;
+	}
+	else
+	{
+		//StopWatch::stopSec();
+		stopSec();
+		_StartTime = std::chrono::steady_clock::now();
+		_running = true;
+
+		return 2; //2 means clock has been retagged
+	}
+
+}
+
+double StopWatch::stopSec()
+{
+	if (_running == true) {
+
+		_EndTime = std::chrono::steady_clock::now();
+		_running = false;
+		return (double)std::chrono::duration_cast<std::chrono::seconds>(_EndTime - _StartTime).count(); //returning time in ms and doubleifying the long longs
+	}
+	else return 0;
+}
