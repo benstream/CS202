e3e686811c93181b85ebd6c51a6df6883a1ee63b
benstream <benjaminstream@yahoo.com>
2021-04-13 21:47:53 -0800

    Starting C-Style-Strings by setting up the function definitions

---
+++ Homework 1/Program 2/c_style_strings.cpp
@@ -0,0 +1,26 @@
+
+
+//that copies a C-style string into memory it allocate dynamically(using new).
+char* strdup(const char*) {
+
+
+
+}
+
+
+// finds the first occurrence of the C - style string x in s.
+char* findx(const char* s, const char* x) {
+
+
+
+}
+
+int main() {
+
+
+
+
+
+
+
+}
