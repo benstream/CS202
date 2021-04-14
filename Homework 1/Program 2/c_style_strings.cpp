70bb4503e5246de66326f479f40544847067bda7
benstream <benjaminstream@yahoo.com>
2021-04-13 23:54:33 -0800

    First run of first function

--- Homework 1/Program 2/c_style_strings.cpp
+++ Homework 1/Program 2/c_style_strings.cpp
@@ -1,10 +1,16 @@
 
+#include <iostream>
+//Benjamin Stream
+//CS202
+//C_STYLE_STRINGS.CPP
+
+
 
 //that copies a C-style string into memory it allocate dynamically(using new).
-char* strdup(const char*) {
-
-
-
+char* strdup(const char* s) {
+	char* tempcopy = new char; 
+	tempcopy = (char*)s;
+	return tempcopy;
 }
 
 
@@ -17,8 +23,12 @@ char* findx(const char* s, const char* x) {
 
 int main() {
 
+	char quad[] = "The quadradic formula is -A plus or minus Square root of B squared minus 4AC all over 2 A";
 
-
+	for (auto i : quad)
+	{
+		std::cout << strdup(quad) << std::endl;
+	}
 
 
 
