6895fe82f3b951a62ea39ff5c50a358f462325ca
benstream <benjaminstream@yahoo.com>
2021-04-14 00:14:02 -0800

    Wrote in first attempt at findx with a test.

--- Homework 1/Program 2/c_style_strings.cpp
+++ Homework 1/Program 2/c_style_strings.cpp
@@ -6,7 +6,7 @@
 
 
 
-//that copies a C-style string into memory it allocate dynamically(using new).
+//copies a C-style string into memory it allocate dynamically(using new).
 //removed 'u' in strpup because the defined function is depreciated
 char* strdp(const char* s) {
 	char* tempcopy = new char; 
@@ -17,17 +17,24 @@ char* strdp(const char* s) {
 
 // finds the first occurrence of the C - style string x in s.
 char* findx(const char* s, const char* x) {
-
-	return 0;
+	while (true) {
+		int i = 0;
+		if (s == x) {
+			printf("We found %s at %d",strdp(s),i);
+			break;
+		}
+		i++;
+	}
+	
 
 }
 
 int main() {
-
+	char quadradic[]= "quadradic";
 	char quad[] = "The quadradic formula is -A plus or minus Square root of B squared minus 4AC all over 2 A";
-
+	printf(quad);
 	std::cout << strdp(quad) << std::endl;
-
+	findx(quadradic, quad);
 
 
 
