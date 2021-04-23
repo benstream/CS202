252c658de5d4745c5bfc69beb579c6d7b007a4e3
benstream <benjaminstream@yahoo.com>
2021-04-23 01:12:56 -0800

    fixed more error with createFreeS();, finished program.

--- Homework 2/Program 2/memory_layout.cpp
+++ Homework 2/Program 2/memory_layout.cpp
@@ -5,63 +5,60 @@ CS202
 */
 #include <iostream>
 
-void memory_layout(const int &i,const int &j,const int &k) {
-    if (&i > &k && &j < &k) // if i>k and j>k ::i>j
-    {
-        std::cout << "The addresses seem to be increasing\n";
-    }
-    else if (&i < &k && &j > &k) // if k>i and k>j ::k>i
-    {
-        std::cout << "The addresses seem to be increasing\n";
-    }
-    else
-    {
-        std::cout << "There seems to be no order detect\n";
-    }
-
-
+void memory_layout(const int& i, const int& j, const int& k) {
+	if (&i > &k && &j > &k) // if i>k and j>k ::i>j
+	{
+		std::cout << "The addresses seem to be decreasing\n";
+	}
+	else if (&k > &i && &j > &i) // if k>i and j>i ::k>i
+	{
+		std::cout << "The addresses seem to be increasing\n";
+	}
+	else
+	{
+		std::cout << "There seems to be no order detected\n";
+	}
 }
-
-
 
 void printPointer(int& i) {
-    int* iptr = &i;
-    // the unary * operator dereferences the pointer
-    std::cout << "i = " << *iptr << "\n";
-    std::cout << "& i = " << iptr << "\n" << std::endl;
-    
+	int* iptr = &i;
+	// the unary * operator dereferences the pointer
+	std::cout << "i = " << *iptr << "\n";
+	std::cout << "&i = " << iptr << "\n";
 }
 
-void createStack(){
-    std::cout << "STACK\n";
-    int s1 = 1, s2 = 1, s3 = 1;
-    printPointer(s1);
-    printPointer(s2);
-    printPointer(s3);
-    memory_layout(s1, s2, s3);
+void createStack() {
+	std::cout << "STACK\n";
+	int s1 = 1, s2 = 1, s3 = 1;
+	printPointer(s1);
+	printPointer(s2);
+	printPointer(s3);
+	memory_layout(s1, s2, s3);
 }
 
 void createStatic() {
-    static int s1 = 1, s2 = 1, s3 = 1;
-    std::cout << "STATIC\n";
-    printPointer(s1);
-    printPointer(s2);
-    printPointer(s3);
-    memory_layout(s1, s2, s3);
+	static int s1 = 1, s2 = 1, s3 = 1;
+	std::cout << "STATIC\n";
+	printPointer(s1);
+	printPointer(s2);
+	printPointer(s3);
+	memory_layout(s1, s2, s3);
 }
 
 void createFreeS() {
-    int* s = new int[1,1,1];
-    std::cout << "FREE STORAGE\n";
-    printPointer(s[0]);
-    printPointer(s[1]);
-    printPointer(s[2]);
-    memory_layout(s[0], s[1], s[2]);
+	int* s = new int[3];
+	s[0] = 1;
+	s[1] = 1;
+	s[2] = 1;
+	std::cout << "FREE STORAGE\n";
+	printPointer(s[0]);
+	printPointer(s[1]);
+	printPointer(s[2]);
+	memory_layout(s[0], s[1], s[2]);
 }
 
 int main() {
-
-    createStack(); // Looks like decreasing, maybe create a function to evaluate for me
-    createStatic(); //Look like increase
-    createFreeS(); //hmmm hard to tell on this one
+	createStack();
+	createStatic();
+	createFreeS();
 }
