d8fbbd0539a12f99001cb6e421b72f821a089c9f
benstream <benjaminstream@yahoo.com>
2021-04-22 20:29:16 -0800

    Finished FIFO and LIFO

--- Homework 2/Program 1/Program 1/linked_list.cpp
+++ Homework 2/Program 1/Program 1/linked_list.cpp
@@ -28,10 +28,11 @@ TEST_CASE("ADDING TO QUEUE") {
 	CHECK(takeiSize != shop.size()); // ensures increasing size 
 	shop.push_back(Cheese);
 	CHECK(takeiSize != shop.size() - 1); // ensures increasing size 
-	
+	shop.push_back(Sticks);
+	CHECK(takeiSize != shop.size() - 2);
 }
 //DELETE QUEUE
-TEST_CASE() {
+TEST_CASE("DELETING THE QUEUE") {
 	int takeiSize = shop.size();
 	shop.pop_front();
 	CHECK(takeiSize == shop.size() + 1); // ensures decreasing size
@@ -40,20 +41,34 @@ TEST_CASE() {
 	CHECK(it->ownerName != Rocks.ownerName); // ensures the first element added is removed from queue
 }
 
-//FIRST IN LAST OUT
-TEST_CASE() {
-	
+//FIRST IN FIRST OUT
+TEST_CASE("FIRST IN FIRST OUT") {
+	int takeiSize = shop.size();
+	std::string takeiName = shop.front().ownerName;
 
+	resources temp = shop.front();
+	shop.push_back(temp);
+	CHECK(temp.ownerName == shop.back().ownerName); //checks to see if front value = last value
+	CHECK(takeiSize != shop.size()); // ensures increasing size (pushes front to back)
+	shop.pop_front();
+	CHECK(takeiSize == shop.size()); // ensure you have the same size that you started with
+	CHECK(takeiName!= shop.front().ownerName); // ensures the first element added is removed from the top
 }
 // STACK LAST IN FIRST OUT
-TEST_CASE() {
-
-
+TEST_CASE("LAST IN FIRST OUT") {
+	int takeiSize = shop.size();
+	std::string takeiName = shop.front().ownerName;
+	resources temp = shop.front();
+	shop.pop_back(); //Removes last value
+	CHECK(takeiSize == shop.size() + 1); // Size should decrease by one from pop_back
+	shop.push_back(temp); //Pushes temp to the back of the list 
+	CHECK(temp.ownerName == shop.back().ownerName); //checks to see if front value = last value
+	CHECK(takeiSize == shop.size()); // ensure you have the same size that you started with
 }
 
 //INSERT AND FIND
 TEST_CASE() {
-
+	shop.sort([](resources i, resources j) {i.item1 > j.item2; }); //Should sort by increaseing # ofitems in slot 1
 
 }
 
