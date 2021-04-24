02788bc89907ba826d51f220c4ca63f15c433a60
benstream <benjaminstream@yahoo.com>
2021-04-24 14:00:30 -0800

    Removed main.cpp and added template test without Rational test.

--- Homework 5/Program 1/myreadwrite.h
+++ Homework 5/Program 1/myreadwrite.h
@@ -10,8 +10,8 @@ void myWrite(std::ofstream& ofile, const number &num) {
 	ofile.write(reinterpret_cast<const char*>(&num), sizeof(num));
 }
 template<typename number>
-void myRead(std::ifstream& ifile, const number &num) {
-	ifile.read(reinterpret_cast<const char*>(&num), sizeof(num));
+void myRead(std::ifstream& ifile, number &num) {
+	ifile.read(reinterpret_cast<char*>(&num), sizeof(num));
 }
 
 #endif //!MYREADWRITE_H
