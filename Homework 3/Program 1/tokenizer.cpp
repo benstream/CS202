a8fcd957e1b62a51c5258d8ae040d43b1ec6c3ed
benstream <benjaminstream@yahoo.com>
2021-04-24 13:13:12 -0800

    Tokenizer.cpp and .h implimented

--- Homework 3/Program 1/tokenizer.cpp
+++ Homework 3/Program 1/tokenizer.cpp
@@ -1,5 +1,22 @@
+#include "tokenizer.h"
 /*
 Benjamin Stream
 tokenizer.cpp
 CS202 HW3
 */
+
+void inputString(const std::string string)
+{
+	tokenizer token;
+	token._reference = string;
+}
+
+
+
+void printTokenizedString(std::istream &is)
+{
+	
+	
+}
+
+
--- Homework 3/Program 1/tokenizer.h
+++ Homework 3/Program 1/tokenizer.h
@@ -3,3 +3,51 @@ Benjamin Stream
 tokenizer.h
 CS202 HW3
 */
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+
+#ifndef TOKENIZER_H
+#define TOKENIZER_H
+
+struct tokenizer {
+	int _line = 0;
+	int _column = 0;
+	std::string _token;
+	std::string _reference;
+};
+
+void inputString(const std::string string);
+
+void printTokenizedString(std::istream& is);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif //!TOKENIZER_H
