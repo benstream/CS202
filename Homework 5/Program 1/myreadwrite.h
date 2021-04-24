d6ee05d7a160810289710ac72f6a33b08eabcecf
benstream <benjaminstream@yahoo.com>
2021-04-24 13:47:26 -0800

    Fixed minor typo in myreadwrite.h

--- Homework 5/Program 1/myreadwrite.h
+++ Homework 5/Program 1/myreadwrite.h
@@ -1,4 +1,6 @@
-#include<istream>
+
+#ifndef MYREADWRITE_H
+#define MYREADWRITE_H
 #include <iostream>
 #include <fstream>
 
@@ -8,6 +10,8 @@ void myWrite(std::ofstream& ofile, const number &num) {
 	ofile.write(reinterpret_cast<const char*>(&num), sizeof(num));
 }
 template<typename number>
-void myRead(std::ofstream& ofile, const number &num) {
-	ofile.read(reinterpret_cast<const char*>(&num), sizeof(num));
+void myRead(std::ifstream& ifile, const number &num) {
+	ifile.read(reinterpret_cast<const char*>(&num), sizeof(num));
 }
+
+#endif //!MYREADWRITE_H
